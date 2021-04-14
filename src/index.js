2716192eb251afc5ab7d9a0ae3e19da6ec557b70
Jongbin Won <jongbinwon@192.168.2.13>
2021-04-15 01:35:06 +0700

    yee

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const e = require('express')
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+const app = express()
+const port = process.env.PORT
+
+
+const multer = require('multer')
+const upload = multer({
+    dest: 'images'
+})
+
+app.post('/upload', upload.single('upload'), (req, res) => {
+    res.send()
+})
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+
+app.listen(port, () => {
+    console.log("server up and running " + port)
+})
